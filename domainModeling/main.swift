f0dff07af6b331cfd883cf307989b7133191e03c
studentuser <studentuser@studentusers-macbook-pro.local>
2015-10-15 13:49:42 -0700

    Initial Commit

---
+++ domainModeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domainModeling
+//
+//  Created by studentuser on 10/15/15.
+//  Copyright © 2015 kartikey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
